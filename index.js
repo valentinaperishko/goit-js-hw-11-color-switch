99777cb49d67003b446ebc7e3978259f798904c2
Valentina Perishko <valentina.perishko@gmail.com>
2020-12-26 18:41:26 -0600

    adds changes

---
+++ index.js
@@ -0,0 +1,33 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+  const startBtn = document.querySelector('[data-action="start"]');
+  const stopBtn = document.querySelector('[data-action="stop"]');
+  const body = document.querySelector('body');
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  
+  let setInt;
+
+  startBtn.addEventListener("click", (num) => {
+    setInt = setInterval(() => {
+        num = randomIntegerFromInterval(0, 5);
+        body.style.backgroundColor = colors[num];
+    }, 1000);
+    startBtn.classList.toggle('nonactive'); 
+  });
+
+  stopBtn.addEventListener("click", () => {
+    startBtn.classList.toggle('nonactive');
+    body.style.backgroundColor = '';  
+    clearInterval(setInt);
+    
+  });
+
