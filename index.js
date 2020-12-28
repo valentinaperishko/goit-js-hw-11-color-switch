de0d8bd109651011edcab1e02885dea7a42e839a
Valentina Perishko <valentina.perishko@gmail.com>
2020-12-28 08:29:11 -0600

    adds formating code

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
-  const startBtn = document.querySelector('[data-action="start"]');
-  const stopBtn = document.querySelector('[data-action="stop"]');
-  const body = document.querySelector('body');
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body');
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-  
-  let setInt;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-  startBtn.addEventListener("click", (num) => {
-    setInt = setInterval(() => {
-        num = randomIntegerFromInterval(0, colors.length - 1);
-        body.style.backgroundColor = colors[num];
-    }, 1000);
-  startBtn.classList.toggle('nonactive'); 
+let setInt;
+
+startBtn.addEventListener('click', num => {
+  setInt = setInterval(() => {
+    num = randomIntegerFromInterval(0, colors.length - 1);
+    body.style.backgroundColor = colors[num];
+  }, 1000);
+  startBtn.classList.toggle('nonactive');
   stopBtn.removeAttribute('disabled');
   startBtn.setAttribute('disabled', true);
-  });
+});
 
-  stopBtn.addEventListener("click", () => {
+stopBtn.addEventListener('click', () => {
   stopBtn.classList.toggle('nonactive');
   startBtn.removeAttribute('disabled');
-  stopBtn.setAttribute('disabled', true);  
-  body.style.backgroundColor = '';  
+  stopBtn.setAttribute('disabled', true);
+  body.style.backgroundColor = '';
   clearInterval(setInt);
-    
-  });
-
+});
